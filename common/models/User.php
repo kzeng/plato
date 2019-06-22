4497dbeabc2023ff2ff405afe3ff155f59efcf27
beesoft <beherochuling@163.com>
2019-06-23 00:17:06 +0800

    change user signup initial status to active

--- common/models/User.php
+++ common/models/User.php
@@ -53,7 +53,7 @@ class User extends ActiveRecord implements IdentityInterface
     public function rules()
     {
         return [
-            ['status', 'default', 'value' => self::STATUS_INACTIVE],
+            ['status', 'default', 'value' => self::STATUS_ACTIVE],
             ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_INACTIVE, self::STATUS_DELETED]],
         ];
     }
