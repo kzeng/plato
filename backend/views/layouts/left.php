27f8b2d7f4be3e0436175f730cbe3eaf6803bb9a
zengkai <zengkai001@qq.com>
2019-06-29 09:23:49 +0800

    mod menu

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -47,70 +47,87 @@
                     //['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug']],
                     //['label' => 'Login', 'url' => ['site/login'], 'visible' => Yii::$app->user->isGuest],
 
-                    // 新增菜单条目，测试用
                     [
-                        'label' => '临时入口1',
-                        'icon' => 'share',
+                        'label' => '馆藏信息',
+                        'icon' => 'university',
                         'url' => '#',
                         'items' => [
-                            ['label' => '用户管理', 'icon' => 'check', 'url' => ['/user1']],
-                            ['label' => '图书馆配置', 'icon' => 'check', 'url' => ['/library']],
-                            ['label' => '读者管理', 'icon' => 'check', 'url' => ['/reader']],
-                            ['label' => '缴纳欠费管理', 'icon' => 'check', 'url' => ['/payment-of-debt']],
                             ['label' => '图书管理', 'icon' => 'check', 'url' => ['/book']],
                             ['label' => '图书副本管理', 'icon' => 'check', 'url' => ['/book-copy']],
-                            ['label' => '借还书管理', 'icon' => 'check', 'url' => ['/borrow-return-books']],
+ 
                         ],
                     ],
 
                     [
-                        'label' => '临时入口2',
-                        'icon' => 'share',
+                        'label' => '流通借阅',
+                        'icon' => 'book',
                         'url' => '#',
                         'items' => [
+                            ['label' => '借还书管理', 'icon' => 'check', 'url' => ['/borrow-return-books']],
+                            ['label' => '阅读室管理', 'icon' => 'check', 'url' => ['/reading-room']],
+                        ],
+                    ],
+
+                    
+                    [
+                        'label' => '流通管理',
+                        'icon' => 'user',
+                        'url' => '#',
+                        'items' => [
+                            ['label' => '读者管理', 'icon' => 'check', 'url' => ['/reader']],
+                            ['label' => '缴纳欠费管理', 'icon' => 'check', 'url' => ['/payment-of-debt']],
+                        ],
+                    ],
+
+                    [
+                        'label' => '系统管理',
+                        'icon' => 'gear',
+                        'url' => '#',
+                        'items' => [
+                            ['label' => '图书馆配置', 'icon' => 'check', 'url' => ['/library']],
+                            ['label' => '用户管理', 'icon' => 'check', 'url' => ['/user1']],
                             ['label' => '馆藏地管理', 'icon' => 'check', 'url' => ['/collection-place']],
                             ['label' => '书商管理', 'icon' => 'check', 'url' => ['/bookseller']],
-                            ['label' => '阅读室管理', 'icon' => 'check', 'url' => ['/reading-room']],
                             ['label' => '违章类型管理', 'icon' => 'check', 'url' => ['/violation-type']],
                             ['label' => '流通类型管理', 'icon' => 'check', 'url' => ['/circulation-type']],
                             ['label' => '读者类型管理', 'icon' => 'check', 'url' => ['/reader-type']],
                             ['label' => '借阅规则配置', 'icon' => 'check', 'url' => ['/borrowing-rules']],
                             ['label' => '条码号序列设置', 'icon' => 'check', 'url' => ['/bar-code']],
                             ['label' => '索书号管理', 'icon' => 'check', 'url' => ['/call-number-rules']],
-
-
-
-                            ['label' => '权限配置', 'icon' => 'file-code-o', 'url' => ['/admin']],
+                            ['label' => '权限配置', 'icon' => 'exclamation-triangle', 'url' => ['/admin']],
                         ],
                     ],
 
+
                     // 新增菜单条目，END
-                    [
-                        'label' => 'Some tools',
-                        'icon' => 'share',
-                        'url' => '#',
-                        'items' => [
-                            ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
-                            ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
-                            [
-                                'label' => 'Level One',
-                                'icon' => 'circle-o',
-                                'url' => '#',
-                                'items' => [
-                                    ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
-                                    [
-                                        'label' => 'Level Two',
-                                        'icon' => 'circle-o',
-                                        'url' => '#',
-                                        'items' => [
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                            ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
-                                        ],
-                                    ],
-                                ],
-                            ],
-                        ],
-                    ]
+                    // [
+                    //     'label' => 'Some tools',
+                    //     'icon' => 'share',
+                    //     'url' => '#',
+                    //     'items' => [
+                    //         ['label' => 'Gii', 'icon' => 'file-code-o', 'url' => ['/gii'],],
+                    //         ['label' => 'Debug', 'icon' => 'dashboard', 'url' => ['/debug'],],
+                    //         [
+                    //             'label' => 'Level One',
+                    //             'icon' => 'circle-o',
+                    //             'url' => '#',
+                    //             'items' => [
+                    //                 ['label' => 'Level Two', 'icon' => 'circle-o', 'url' => '#',],
+                    //                 [
+                    //                     'label' => 'Level Two',
+                    //                     'icon' => 'circle-o',
+                    //                     'url' => '#',
+                    //                     'items' => [
+                    //                         ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
+                    //                         ['label' => 'Level Three', 'icon' => 'circle-o', 'url' => '#',],
+                    //                     ],
+                    //                 ],
+                    //             ],
+                    //         ],
+                    //     ],
+                    // ]
+
+
                 ])
             ]
         ) ?>
