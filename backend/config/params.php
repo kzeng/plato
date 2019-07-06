c5964dc7324a8ef5acc3de5cbea45a22fbf1ee5d
zengkai <zengkai001@qq.com>
2019-07-06 15:27:57 +0800

    mod a samll config

--- backend/config/params.php
+++ backend/config/params.php
@@ -1,4 +1,5 @@
 <?php
 return [
     'adminEmail' => 'admin@example.com',
+    'webapp_name' => '智慧云图书馆',
 ];
--- backend/views/layouts/main.php
+++ backend/views/layouts/main.php
@@ -33,7 +33,9 @@ if (Yii::$app->controller->action->id === 'login') {
         <meta charset="<?= Yii::$app->charset ?>"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <?= Html::csrfMetaTags() ?>
-        <title><?= Html::encode($this->title) ?></title>
+        <title>
+        <?= Html::encode(Yii::$app->params['webapp_name']) ?>
+        </title>
         <?php $this->head() ?>
     </head>
     <body class="hold-transition skin-blue sidebar-mini">
